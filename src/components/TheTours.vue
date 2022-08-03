85b58c8d66e589a207503193e65f054e85bbcb5d
Veniamin Polienko <kasaklalita@mail.ru>
2022-08-04 02:58:11 +0300

    Added and styled the tours section

--- src/components/TheTours.vue
+++ src/components/TheTours.vue
@@ -2,7 +2,10 @@
 	<div class="tours">
 		<wide-wrapper>
 			<div class="tours__wrap">
-				<tour-card></tour-card>
+				<tour-card
+					v-for="tour in tours"
+					:key="tour.title"
+					:tour="tour"></tour-card>
 			</div>
 		</wide-wrapper>
 	</div>
@@ -14,10 +17,30 @@
 		name: "TheTours",
 		components: {
 			TourCard
+		},
+		data() {
+			return {
+				tours: [
+					{
+						reversed: false,
+						title: 'Prefecture in Focus: Tottori',
+						description: 'Jump off balcony, onto stranger\'s head. Chase ball of string hide when guests come over. Being gorgeous with belly side up i could pee on this if i had the energy but under the bed, for attack the child, open the door, '
+					},
+					{
+						reversed: true,
+						title: 'Featured Neighborhood: Kyoto’s Arashiyama',
+						description: 'Jump off balcony, onto stranger\'s head. Chase ball of string hide when guests come over. Being gorgeous with belly side up i could pee on this if i had the energy but under the bed, for attack the child, open the door, '
+					},
+				]
+			}
 		}
 	}
 </script>
 
 <style scoped>
-
+	.tours__wrap {
+		display: flex;
+		flex-direction: column;
+		gap: 75px;
+	}
 </style>
--- src/components/TourCard.vue
+++ src/components/TourCard.vue
@@ -1,26 +1,52 @@
 <template>
-	<div class="tour">
+	<div v-if="!tour['reversed']" class="tour">
 		<div class="tour__pic">
 			<img src="../assets/tour-1.jpg" alt="" class="tour__thumb">
 		</div>
 		<div class="tour__info">
-			<h3 class="tour__title">
-				Prefecture in Focus: Tottori
-			</h3>
-			<p class="tour_description">
-				Jump off balcony, onto stranger's head. Chase ball of string hide when guests come over. Being gorgeous with belly side up i could pee on this if i had the energy but under the bed, for attack the child, open the door,
-			</p>
-
+			<h3 class="tour__title">{{tour['title']}}</h3>
+			<p class="tour__description">{{tour['description']}}</p>
+			<see-more-link to="" content="VIEW PREFECTURE"></see-more-link>
+		</div>
+	</div>
+	<div v-else class="tour">
+		<div class="tour__info">
+			<h3 class="tour__title">{{tour['title']}}</h3>
+			<p class="tour__description">{{tour['description']}}</p>
+			<see-more-link to="" content="VIEW PREFECTURE"></see-more-link>
+		</div>
+		<div class="tour__pic">
+			<img src="../assets/tour-1.jpg" alt="" class="tour__thumb">
 		</div>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: "TourCard"
+		name: "TourCard",
+		props: ['tour']
 	}
 </script>
 
 <style scoped>
-
+	.tour {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		grid-gap: 6.75rem;
+		align-items: center;
+	}
+	.tour__info {
+		display: flex;
+		flex-direction: column;
+		gap: 3rem;
+		padding: 0 2rem;
+	}
+	.tour__title {
+		font-weight: 500;
+		font-size: 1.8rem;
+		line-height: 40px;
+	}
+	.tour__description {
+		line-height: 1.5rem;
+	}
 </style>
